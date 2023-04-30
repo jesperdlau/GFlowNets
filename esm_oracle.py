7662428acb1a63f52cfc39f27d8f307655a3e6f2
jesperdlau <jesperdlau@gmail.com>
2023-04-30 22:49:56 +0200

    Working transformer oracle for GFP

---
+++ esm_oracle.py
@@ -0,0 +1,40 @@
+import numpy as np
+import torch
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
+
+#tokenizer = AutoTokenizer.from_pretrained("jiuyiyuyi/esm2_t12_35M_UR50D-Fluorescence-230313")
+tokenizer = AutoTokenizer.from_pretrained("GFlowNets/data/gfp/esm_tokenizer") # Local 
+
+#model = AutoModelForSequenceClassification.from_pretrained("jiuyiyuyi/esm2_t12_35M_UR50D-Fluorescence-230313")
+model = AutoModelForSequenceClassification.from_pretrained("GFlowNets/data/gfp/esm_model") # Local 
+
+
+
+def tokens2tokens(torch_batch):
+    pass
+
+
+def oracle(batch):
+    with torch.no_grad():
+        pred = model(input_ids=batch['input_ids'], attention_mask=batch['attention_mask']).logits
+    return pred.detach().numpy()
+
+
+
+if __name__ == "__main__":
+    from datasets import load_dataset, get_dataset_split_names, get_dataset_config_names, Dataset
+
+    dataset = load_dataset("fluorescence")
+    dataset_train = dataset["train"]
+    dataset_eval = dataset["validation"]
+    dataset_test = dataset["test"]
+
+    # Batch pred
+    seq = dataset_test['primary'][:10]
+    #labels = torch.tensor(dataset_test['log_fluorescence'][:10])
+    tok = tokenizer(seq, max_length=300, padding='max_length', return_tensors='pt')
+
+    pred = oracle(tok)
+    print(pred)
+    print()
