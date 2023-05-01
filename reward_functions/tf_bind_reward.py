acfe25bea045ff14bcca5931d9aefd042c776cb9
RasmusTorp <s214597@dtu.dk>
2023-05-01 12:28:01 +0200

    Fixed bug with dataset implemenation

--- reward_functions/tf_bind_reward.py
+++ reward_functions/tf_bind_reward.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 # import pandas as pd
 # import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, TensorDataset
 
 DATA_FOLDER = "GFlowNets/data/"
 
@@ -13,10 +13,10 @@ DATA_FOLDER = "GFlowNets/data/"
 DATA_FOLDER = "data/"
 
 SEED = 42
-TRAIN_SIZE = 1/5
-EPOCHS = 50
+TRAIN_SIZE = 4/5
+EPOCHS = 30
 BATCH_SIZE = 100
-LEARNING_RATE = 0.0001
+LEARNING_RATE = 0.001
 
 device = (
     "cuda"
@@ -25,7 +25,7 @@ device = (
     if torch.backends.mps.is_available()
     else "cpu"
 )
-print(f"\nUsing {device} device")
+print(f"\nUsing {device} device")   
 
 
 X = np.load(DATA_FOLDER + "tf_bind_8/SIX6_REF_R1/tf_bind_8-x.npy")
@@ -37,17 +37,17 @@ y_train = torch.tensor(y_train, dtype=torch.float32).reshape(-1, 1)
 X_test = torch.tensor(X_test, dtype=torch.float32)
 y_test = torch.tensor(y_test, dtype=torch.float32).reshape(-1, 1)
 
-class SequenceDataset(Dataset):
+# class SequenceDataset(Dataset):
 
-    def __init__(self,X,y):
-        self.X = X
-        self.y = y
+#     def __init__(self,X,y):
+#         self.X = X
+#         self.y = y
     
-    def __getitem__(self, index):
-        return self.X[index], y[index]
+#     def __getitem__(self, index: int):
+#         return self.X[index], y[index]
     
-    def __len__(self):
-        return len(self.X)
+#     def __len__(self) -> int:
+#         return len(self.X)
 
 
 class TFBindReward(nn.Module):
@@ -59,21 +59,23 @@ class TFBindReward(nn.Module):
         # self.opt = optim.Adam(model.parameters(), lr=lr)
         # layers = []
         
+        self.model = nn.Sequential(
+                nn.Linear(8, 100),
+                nn.ReLU(),
+                nn.Linear(100, 100),
+                nn.ReLU(),
+                nn.Linear(100, 100),
+                nn.ReLU(),
+                nn.Linear(100, 1))
+
         # self.model = nn.Sequential(
-        #         nn.Linear(8, 16),
+        #         nn.Linear(8, 24),
         #         nn.ReLU(),
-        #         nn.Linear(16, 6),
+        #         nn.Linear(24, 12),
+        #         nn.ReLU(),
+        #         nn.Linear(12, 6),
         #         nn.ReLU(),
         #         nn.Linear(6, 1))
-
-        self.model = nn.Sequential(
-                nn.Linear(8, 24),
-                nn.ReLU(),
-                nn.Linear(24, 12),
-                nn.ReLU(),
-                nn.Linear(12, 6),
-                nn.ReLU(),
-                nn.Linear(6, 1))
         
         # self.input = nn.Linear(8,24)
         # self.output = nn.Linear(n_layers,1)
@@ -91,43 +93,88 @@ class TFBindReward(nn.Module):
         return self.model(x)
         
 # inspired by pytorch documentation: https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html
-def inner_train(dataloader, model, loss_fn, optimizer, verbose = True):
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
-        # Compute prediction and loss
-        pred = model(X)
-        loss = loss_fn(pred, y)
+# def inner_train(dataloader, model, loss_fn, optimizer, verbose = True):
+#     size = len(dataloader.dataset)
+    
+#     model.train()
+#     for batch, (X, y) in enumerate(dataloader):
+#         # Compute prediction and loss
+#         pred = model(X)
+#         loss = loss_fn(pred, y)
+        
+#         # Backpropagation
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+        
 
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
 
-
-        if batch % 25 == 0 and verbose == True:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+#         if batch % 25 == 0 and verbose == True:
+#             loss, current = loss.item(), (batch + 1) * len(X)
+#             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    
 
 # Inspired by pytorch documentation: https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html
-def test_loop(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    test_loss, correct = 0, 0
-
-    with torch.no_grad():
-        for X, y in dataloader:
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            # correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-
-    print(f"Mean squared error: {test_loss / size}")
+# def test_loop(dataloader, model, loss_fn):
+#     size = len(dataloader.dataset)
+#     num_batches = len(dataloader)
+#     test_loss, correct = 0, 0
 
 
-def train_model(epochs, train_DL,test_DL, model, loss_fn, optimizer,save_as = None):
+#     model.eval()
+#     with torch.no_grad():
+#         for X, y in dataloader:
+#             pred = model(X)
+#             test_loss += loss_fn(pred, y).item()
+#             # correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+#     print(f"Mean squared error: {test_loss / size}")
+
+
+# def train_model(epochs, train_DL,test_DL, model, loss_fn, optimizer,save_as = None):
+#     for epoch in range(epochs):
+#         print(f"\nEpoch number: {epoch + 1}")
+#         inner_train(train_DL,model,loss_fn,optimizer)
+#         test_loop(test_DL,model,loss_fn)
+
+#     if save_as:
+#         torch.save(model.state_dict(), save_as + ".pth")
+
+
+def train_model(epochs, train_DL,test_DL, model, loss_fn, optimizer,save_as = None,verbose=True):
     for epoch in range(epochs):
-        print(f"\nEpoch number: {epoch}")
-        inner_train(train_DL,model,loss_fn,optimizer)
-        test_loop(test_DL,model,loss_fn)
+        size = len(train_DL.dataset)
+
+        #model trainging one epoch
+        model.train()
+        for batch, (X, y) in enumerate(train_DL):
+            # Compute prediction and loss
+            pred = model(X)
+            loss = loss_fn(pred, y)
+            
+            # Backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if batch % 25 == 0 and verbose == True:
+                loss, current = loss.item(), (batch + 1) * len(X)
+                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        
+        # Test set evaluation
+        model.eval()
+        size = len(test_DL.dataset)
+        num_batches = len(test_DL)
+        test_loss, correct = 0, 0
+
+        with torch.no_grad():
+            for X, y in test_DL:
+                pred = model(X)
+                test_loss += loss_fn(pred, y).item()
+                # correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+        print(f"Mean squared error: {test_loss / size}\n")
+
 
     if save_as:
         torch.save(model.state_dict(), save_as + ".pth")
@@ -137,8 +184,11 @@ if __name__ == "__main__":
     loss = nn.MSELoss()
     opt = optim.Adam(model.parameters(), lr=LEARNING_RATE)
     
-    trainSet = SequenceDataset(X_train,y_train)
-    testSet = SequenceDataset(X_test,y_test)
+    # trainSet = SequenceDataset(X_train,y_train)
+    # testSet = SequenceDataset(X_test,y_test)
+
+    trainSet = TensorDataset(X_train,y_train)
+    testSet = TensorDataset(X_test,y_test)
 
     train_dataLoader = DataLoader(trainSet,batch_size=BATCH_SIZE,shuffle=True)
     test_dataLoader =  DataLoader(testSet,batch_size=BATCH_SIZE,shuffle=True)
