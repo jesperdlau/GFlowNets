e03e29b17d0040a79577b944882064fdf6304662
RasmusTorp <s214597@dtu.dk>
2023-06-05 10:24:50 +0200

    helper function to train models

---
+++ reward_functions/train_reward_function.py
@@ -0,0 +1,39 @@
+
+import torch
+
+def train_model(epochs, train_DL,test_DL, model, loss_fn, optimizer,save_as = None,verbose=True):
+    for epoch in range(epochs):
+        size = len(train_DL.dataset)
+
+        #model trainging one epoch
+        model.train()
+        for batch, (X, y) in enumerate(train_DL):
+            # Compute prediction and loss
+            pred = model(X)
+            loss = loss_fn(pred, y)
+            
+            # Backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if batch % 25 == 0 and verbose == True:
+                loss, current = loss.item(), (batch + 1) * len(X)
+                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        
+        # Test set evaluation
+        model.eval()
+        size = len(test_DL.dataset)
+        num_batches = len(test_DL)
+        test_loss, correct = 0, 0
+
+        with torch.no_grad():
+            for X, y in test_DL:
+                pred = model(X)
+                test_loss += loss_fn(pred, y).item()
+                # correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+        print(f"Mean squared error: {test_loss / size}\n")
+
+    if save_as:
+        torch.save(model.state_dict(), save_as + ".pth")
