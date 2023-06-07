dce311b1736601d78ac0df178755a68e667194bd
jesperdlau <jesperdlau@gmail.com>
2023-06-07 20:27:29 +0200

    added simple loss plot

--- tfbind8_main.py
+++ tfbind8_main.py
@@ -7,14 +7,17 @@ from models.train import train
 from models.tfbind8_model import GFlowNet
 from reward_functions.tf_bind_reward_1hot import TFBindReward1HOT
 from reward_functions import torch_helperfunctions as help
+from utilities.loss_plot import loss_plot
+
 
 # Hyperparameters
-HIDDEN_SIZE = 512
+SEQ_LEN = 8 # Shouldn't need to change
+HIDDEN_SIZE = 1024
 LEARNING_RATE = 3e-4
-SEQ_LEN = 8
-NUM_EPISODES = 100
-UPDATE_FREQ = 4
-PATH = "models/model.tar"
+NUM_EPISODES = 1000
+UPDATE_FREQ = 10
+PATH = "models/test_model_1.tar"
+PLOT_PATH = "models/test_loss_plot_1.png"
 
 # Set device
 device = help.set_device()
@@ -28,9 +31,11 @@ reward_func = TFBindReward1HOT()
 reward_path = "data/tf_bind_8/SIX6_REF_R1/TFBind_1hot_test.pth"
 reward_func.load_state_dict(torch.load(reward_path))
 
-# Train model. It will save
-train(model, optimizer, reward_func, SEQ_LEN, NUM_EPISODES, UPDATE_FREQ, device, PATH)
+# Train model and save checkpoint to PATH
+train(model, optimizer, reward_func, SEQ_LEN, NUM_EPISODES, UPDATE_FREQ, device, PATH, hot_start=True)
 
+# Plot of loss saved to PLOT_PATH
+loss_plot(PATH, save_path=PLOT_PATH)
 
 
 
