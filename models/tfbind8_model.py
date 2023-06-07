Fixed a nasty bug raised by gradient computation

models/tfbind8_model.py
@@ -19,7 +19,7 @@ class GFlowNet(nn.Module):
         return one_hot
     
     def step(self, i, state, action):
-        next_state = state
+        next_state = state.clone() # If not .clone, raises "RuntimeError: one of the variables needed for gradient computation has been modified by an inplace operation:"
         next_state[(4*i + action)] = 1.
         return next_state
     
@@ -29,7 +29,7 @@ class GFlowNet(nn.Module):
     
 if __name__ == "__main__":
     model = GFlowNet(512)
-    
+
     seq = "ACGT"
     x = model.seq_to_one_hot(seq)
     print(f"{x=}")

models/train.py
@@ -1,18 +1,22 @@
 from torch.distributions.categorical import Categorical
 import torch
+# Enable anomaly detection
+torch.autograd.set_detect_anomaly(True)
 
-def train(model, optimizer, reward_func, seq_len = 8, num_episodes = 100, update_freq = 4, device = "cpu", path = None):
+def train(model, optimizer, reward_func, seq_len = 8, num_episodes = 100, update_freq = 4, device = "cpu", path = None, hot_start = False):
     """
     Trains a given model using policy gradient with a given reward function.
 
     Args:
     - model: a PyTorch model used for training
-    - opt: a PyTorch optimizer used for training
+    - optimizer: a PyTorch optimizer used for training
     - reward_func: a function that calculates the reward for a given state
     - seq_len: an int representing the length of each sequence
     - num_episodes: an int representing the number of episodes to train for
     - update_freq: an int representing the frequency of updating the model
     - device: a PyTorch device to use for training
+    - path: a string representing the path to save the model to
+    - hot_start: a bool indicating whether to load the model from a checkpoint
 
     Returns:
     - None
@@ -25,26 +29,25 @@ def train(model, optimizer, reward_func, seq_len = 8, num_episodes = 100, update
     minibatch_loss = 0
     start_episode = 0
 
-    # If path exists, load model and optimizer
-    if path:
-        checkpoint = torch.load(checkpoint)
+    # If hot_start is true, load model and optimizer from checkpoint
+    if hot_start == True:
+        checkpoint = torch.load(path)
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         start_episode = checkpoint['episode']
-        minibatch_loss = checkpoint['minibatch_loss']
         losses = checkpoint['losses']
 
+    # Move model and reward function to device
     reward_func.to(device)
     model.to(device)
     model.train()
 
     for episode in range(start_episode, start_episode + num_episodes):
         # Initialize empty state (as one-hot) and trajectory flow
-        state = torch.zeros(32, dtype=torch.float)
+        state = torch.zeros(32, dtype=torch.float, device=device)
         trajectory_flow = []
 
         # Predict edge flow from initial state
-        state.to(device)
         edge_flow_prediction = model(state)
 
         for i in range(seq_len):
@@ -53,10 +56,9 @@ def train(model, optimizer, reward_func, seq_len = 8, num_episodes = 100, update
 
             # Sample action from policy
             action = Categorical(probs=policy).sample() 
-            #action.to(device) # TODO: Is it necessary to send action to device?
+            # action.to(device) # TODO: Is it necessary to send action to device?
 
             # Take action and get new state
-            #new_state = state + [model.keys[action]]
             new_state = model.step(i, state, action)
             new_state.to(device)
 
@@ -71,9 +73,7 @@ def train(model, optimizer, reward_func, seq_len = 8, num_episodes = 100, update
                 
             # When sequence is complete, get reward and set flow prediction to zero
             else:
-                #reward = reward_func(new_state)
-                #reward = torch.tensor(reward_func(seq_to_one_hot(new_state))).float()[0] 
-                reward = reward_func(new_state) # TODO: Make sure this works
+                reward = reward_func(new_state) 
                 terminal_rewards.append(reward)
                 edge_flow_prediction = torch.zeros(4)
                 
@@ -87,19 +87,20 @@ def train(model, optimizer, reward_func, seq_len = 8, num_episodes = 100, update
         total_trajectory_flow.append(sum(trajectory_flow))
         sampled_sequences.append(state) # TODO: Possibly go from one-hot to id/char? (And save both one-hot and chars..?)
 
-        print(f"{episode=}, {minibatch_loss=}")
+        print(f"{episode=}, {minibatch_loss.item()=:.2f}")
 
         # Perform training step
         if episode % update_freq == 0:
-            losses.append(minibatch_loss.item())
+            optimizer.zero_grad()
             minibatch_loss.backward()
             optimizer.step()
-            optimizer.zero_grad()
+            
+            # Update losses and reset minibatch_loss
+            losses.append(minibatch_loss.item())
             minibatch_loss = 0
-            print(f"Performed optimization step")
+            #print(f"Performed optimization step")
 
     # Save checkpoint
-    #torch.save(model.state_dict(), 'model_weights.pth')
     if path:
         torch.save({
                     'episode': episode,