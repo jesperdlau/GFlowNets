79073271df73f8c68d8b7836fcb4aaf6d33e3546
RasmusTorp <s214597@dtu.dk>
2023-06-08 16:48:37 +0200

    gfn sample function

--- models/tfbind8_model.py
+++ models/tfbind8_model.py
@@ -27,24 +27,39 @@ class GFlowNet(nn.Module):
         F = self.mlp(x).exp()
         return F
     
-    def sample(self):
-        pass
-    
+    def sample(self, N):
+        sequences = []
+
+        for seqN in range(N):
+            sequence = torch.zeros(32, dtype=torch.float)
+            
+            for i in range(8):
+                action_distribution = self.mlp(sequence)
+                
+                action_distribution = F.softmax(action_distribution) # Ved ikke om der skal softmaxes, men ellers kan der komme negative værdier
+                action = np.random.choice(4, p=action_distribution.detach().numpy())
+                sequence = self.step(i, sequence, action)
+            
+            # sequences[seqN] = sequence.numpy()
+            sequences.append(sequence)
+        
+        return torch.stack(sequences, dim = 0)
+        
 if __name__ == "__main__":
     model = GFlowNet(512)
 
-    seq = "ACGT"
-    x = model.seq_to_one_hot(seq)
-    print(f"{x=}")
-    x2 = model.step(5, x, 2)
-    print(f"{x2=}")
+    # seq = "ACGT"
+    # x = model.seq_to_one_hot(seq)
+    # print(f"{x=}")
+    # x2 = model.step(5, x, 2)
+    # print(f"{x2=}")
 
-    seq2 = ""
-    x3 = model.seq_to_one_hot(seq2)
-    print(f"{x3=}")
-    x4 = model.step(0, x3, 2)
-    print(f"{x4=}")
+    # seq2 = ""
+    # x3 = model.seq_to_one_hot(seq2)
+    # print(f"{x3=}")
+    # x4 = model.step(0, x3, 2)
+    # print(f"{x4=}")
     
-
+    samples = model.sample(50)
 
     print()
