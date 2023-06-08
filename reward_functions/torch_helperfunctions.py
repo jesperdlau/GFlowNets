5bcafa944e2bc2d0f91048cbb07ad3ab41d5655d
RasmusTorp <s214597@dtu.dk>
2023-06-08 16:16:25 +0200

    Earlystopping implemented

--- reward_functions/torch_helperfunctions.py
+++ reward_functions/torch_helperfunctions.py
@@ -56,3 +56,59 @@ def train_model(epochs:int, train_DL:DataLoader,test_DL:DataLoader, model, loss_
 
     if save_as:
         torch.save(model.state_dict(), save_as + ".pth")
+
+
+def train_model_earlystopping(epochs:int, train_DL:DataLoader, test_DL:DataLoader, model, loss_fn, optimizer, save_as=None, verbose=True, patience=5):
+    best_loss = float('inf')
+    counter = 0
+
+    for epoch in range(epochs):
+        size = len(train_DL.dataset)
+        print(f"epoch number: {epoch}")
+
+        # Model training one epoch
+        model.train()
+        for batch, (X, y) in enumerate(train_DL):
+            # Compute prediction and loss
+            pred = model(X)
+            loss = loss_fn(pred, y)
+
+            # Backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if batch % 25 == 0 and verbose:
+                loss_val, current = loss.item(), (batch + 1) * len(X)
+                print(f"loss: {loss_val:>7f}  [{current:>5d}/{size:>5d}]")
+        
+        # Test set evaluation
+        model.eval()
+        size = len(test_DL.dataset)
+        num_batches = len(test_DL)
+        test_loss = 0
+
+        with torch.no_grad():
+            for X, y in test_DL:
+                pred = model(X)
+                test_loss += loss_fn(pred, y).item()
+
+        mean_squared_error = test_loss / size
+        print(f"Mean squared error: {mean_squared_error}\n")
+
+        # Check for early stopping
+        if mean_squared_error < best_loss:
+            best_loss = mean_squared_error
+            best_model_state = model.state_dict()
+            counter = 0
+        else:
+            counter += 1
+            if counter >= patience:
+                print(f"Validation loss hasn't improved for {counter} epochs. Stopping early.")
+                print(f"Model saved after {epoch} epochs")
+                break
+
+    if save_as:
+        torch.save(best_model_state, save_as + ".pth")
+    else:
+        model.load_state_dict(best_model_state)
