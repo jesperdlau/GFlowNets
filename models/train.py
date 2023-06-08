929e4ecd9ca45e2cd3c7c883df6d3db589cc0e97
jesperdlau <jesperdlau@gmail.com>
2023-06-08 19:10:28 +0200

    Minor fixes relevant to device, verbose, load

--- models/train.py
+++ models/train.py
@@ -3,7 +3,7 @@ import torch
 # Enable anomaly detection
 torch.autograd.set_detect_anomaly(True)
 
-def train(model, optimizer, reward_func, seq_len = 8, num_episodes = 100, update_freq = 4, device = "cpu", path = None, hot_start = False):
+def train(model, optimizer, reward_func, seq_len = 8, num_episodes = 100, update_freq = 4, model_path = None, reward_path = None, device = "cpu", hot_start = False, verbose = False):
     """
     Trains a given model using policy gradient with a given reward function.
 
@@ -15,8 +15,11 @@ def train(model, optimizer, reward_func, seq_len = 8, num_episodes = 100, update
     - num_episodes: an int representing the number of episodes to train for
     - update_freq: an int representing the frequency of updating the model
     - device: a PyTorch device to use for training
-    - path: a string representing the path to save the model to
+    - model_path: a string representing the path to save the model to, or load from if hot_start
+    - reward_path: a string representing the path to load the reward function from
+    - device: the device where the model, optimizer, and reward function are sent to (default is "cpu")
     - hot_start: a bool indicating whether to load the model from a checkpoint
+    - verbose: a bool indicating whether to print info about each episode
 
     Returns:
     - None
@@ -26,26 +29,32 @@ def train(model, optimizer, reward_func, seq_len = 8, num_episodes = 100, update
     total_trajectory_flow = []
 
     losses = []
-    minibatch_loss = 0
+    minibatch_loss = torch.zeros(1)
     start_episode = 0
 
+    # Move model and reward function to device
+    reward_func.to(device)
+    reward_func.load_state_dict(torch.load(reward_path, map_location=device))
+
+    model.to(device)
+    model.train()
+
     # If hot_start is true, load model and optimizer from checkpoint
-    if hot_start == True:
+    if hot_start:
         try:
-            checkpoint = torch.load(path)
-            model.load_state_dict(checkpoint['model_state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            start_episode = checkpoint['episode']
+            checkpoint = torch.load(model_path)
+            model_state_dict = checkpoint['model_state_dict']
+            model.load_state_dict(model_state_dict)
+            optimizer_state_dict = checkpoint['optimizer_state_dict']
+            optimizer.load_state_dict(optimizer_state_dict)
+            start_episode = checkpoint['episode'] 
+            minibatch_loss = checkpoint['minibatch_loss']
             losses = checkpoint['losses']
         except:
             print("Could not load checkpoint. Starting from scratch.")
 
-    # Move model and reward function to device
-    reward_func.to(device)
-    model.to(device)
-    model.train()
 
-    for episode in range(start_episode, start_episode + num_episodes):
+    for episode in range(start_episode + 1, start_episode + num_episodes + 1):
         # Initialize empty state (as one-hot) and trajectory flow
         state = torch.zeros(32, dtype=torch.float, device=device)
         trajectory_flow = []
@@ -90,28 +99,33 @@ def train(model, optimizer, reward_func, seq_len = 8, num_episodes = 100, update
         total_trajectory_flow.append(sum(trajectory_flow))
         sampled_sequences.append(state) # TODO: Possibly go from one-hot to id/char? (And save both one-hot and chars..?)
 
-        # print(f"{episode=}, {minibatch_loss.item()=:.2f}")
+        if verbose:
+            print(f"{episode=},\t {minibatch_loss.item()=:.2f}")
 
         # Perform training step
-        if episode % update_freq == 0:
+        if episode % update_freq == 0 and episode != 0:
             optimizer.zero_grad()
             minibatch_loss.backward()
             optimizer.step()
             
             # Update losses and reset minibatch_loss
             losses.append(minibatch_loss.item())
-            minibatch_loss = 0
-            # print(f"Performed optimization step")
+            minibatch_loss = torch.zeros(1)
+            if verbose:
+                print(f"Performed optimization step")
 
     # Save checkpoint
-    if path:
+    if model_path:
+        model_state_dict = model.state_dict()
+        model_state_dict_cpu = {k: v.cpu() for k, v in model_state_dict.items()}
+        optimizer_state_dict = optimizer.state_dict()
         torch.save({
                     'episode': episode,
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict(),
-                    'latest_minibatch_loss': minibatch_loss,
+                    'model_state_dict': model_state_dict_cpu,
+                    'optimizer_state_dict': optimizer_state_dict,
+                    'minibatch_loss': minibatch_loss.item(), # Is this necessary?
                     'losses': losses
-                    }, path)
-        print(f"Saved model to {path}")
+                    }, model_path)
+        print(f"Saved model to {model_path}")
 
 
