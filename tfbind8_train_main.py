eaba8782d69487f8ae1a0c952a3d437b894e8df9
jesperdlau <jesperdlau@gmail.com>
2023-06-12 15:12:52 +0200

    added name_of_run to simplify

--- tfbind8_train_main.py
+++ tfbind8_train_main.py
@@ -8,23 +8,27 @@ from reward_functions.tf_bind_reward_1hot import TFBindReward1HOT
 from reward_functions import torch_helperfunctions as help
 
 # Hyperparameters
-SEQ_LEN = 8
+NAME_OF_RUN = "100mc_test"
+NUM_EPISODES = 100 # Should be 5000
+VERBOSE = True
+
+#SEQ_LEN = 8
 HIDDEN_SIZE = 2048
 N_HIDDEN_LAYERS = 2
 LEARNING_RATE = 10e-5
-NUM_EPISODES = 100 # Should be 5000
 UPDATE_FREQ = 32
-VERBOSE = True
-HOT_START = False
 BETAS = (0.9, 0.999)
+HOT_START = False
 
 # HPC
-# MODEL_PATH = "/zhome/2e/b/169155/GFlowNet/tfbind8/model/test_model_2048.tar"
 # REWARD_PATH = "/zhome/2e/b/169155/GFlowNet/tfbind8/reward/tfbind_reward_model_1.pt"
+# MODEL_PATH = "/zhome/2e/b/169155/GFlowNet/tfbind8/model/test_model_2048.tar"
 
-# Local
-MODEL_PATH = "models/saved_models/test_model_2048.tar"
-REWARD_PATH = "models/saved_models/tfbind_reward_model_1.pt"
+# Load path
+REWARD_PATH = "models/saved_models/tfbind8_reward_model_" + NAME_OF_RUN + ".pt"
+
+# Save path
+MODEL_PATH = "models/saved_models/tfbind8_gflow_model_" + NAME_OF_RUN + ".tar"
 
 # Initialize 
 device = help.set_device()
