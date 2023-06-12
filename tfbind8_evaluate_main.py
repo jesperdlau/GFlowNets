ff04debd4f905d2e9b8fb492aee3d7a5727a2ba4
jesperdlau <jesperdlau@gmail.com>
2023-06-12 15:13:25 +0200

    Improved evaluation scripts

--- tfbind8_evaluate_main.py
+++ tfbind8_evaluate_main.py
@@ -11,48 +11,51 @@ import pandas as pd
 #from reward_functions.tf_bind_reward_1hot import TFBindReward1HOT
 from reward_functions import torch_helperfunctions as help
 #from utilities.plot_functions import loss_plot
-from evaluation.evaluation import evaluate_modelsampling
+from evaluation.evaluation import evaluate_modelsampling, get_top20percent, evaluate_batches
 #from utilities.transformer import Transformer
 
 
 # Hyperparameters
+NAME_OF_RUN = "100mc_test"
 VERBOSE = True
 
 # Data
 X_TRAIN_PATH = "data/tf_bind_8/SIX6_REF_R1/tf_bind_1hot_X_train.pt"
 
-# Load paths
-RANDOM_SAMPLES_PATH = "inference/tfbind8_random_samples_400.pt"
-RANDOM_REWARDS_PATH = "inference/tfbind8_random_rewards_400.pt"
-GFLOW_SAMPLES_PATH = "inference/tfbind8_gflow_samples_400.pt"
-GFLOW_REWARDS_PATH = "inference/tfbind8_gflow_rewards_400.pt"
-#MCMC_SAMPLES_PATH = "inference/tfbind8_mcmc_samples_400.pt"
-#MCMC_REWARDS_PATH = "inference/tfbind8_mcmc_rewards_400.pt"
+# Load paths # TODO: Import from shared config script?
+RANDOM_SAMPLES_PATH = "inference/tfbind8_random_samples_" + NAME_OF_RUN + ".pt"
+RANDOM_REWARDS_PATH = "inference/tfbind8_random_rewards_" + NAME_OF_RUN + ".pt"
+GFLOW_SAMPLES_PATH = "inference/tfbind8_gflow_samples_" + NAME_OF_RUN + ".pt"
+GFLOW_REWARDS_PATH = "inference/tfbind8_gflow_rewards_" + NAME_OF_RUN + ".pt"
+MCMC_SAMPLES_PATH = "inference/tfbind8_mcmc_samples_" + NAME_OF_RUN + ".pt"
+MCMC_REWARDS_PATH = "inference/tfbind8_mcmc_rewards_" + NAME_OF_RUN + ".pt"
 
-# Save paths
-RANDOM_METRICS_PATH = "inference/tfbind8_random_metrics_400.npy"
-GFLOW_METRICS_PATH = "inference/tfbind8_gflow_metrics_400.npy"
-#MCMC_METRICS_PATH = "inference/tfbind8_mcmc_metrics_400.npy"
+# Save paths TODO: Import from shared config script?
+RANDOM_METRICS_PATH = "inference/tfbind8_random_metrics_" + NAME_OF_RUN + ".npy"
+GFLOW_METRICS_PATH = "inference/tfbind8_gflow_metrics_" + NAME_OF_RUN + ".npy"
+MCMC_METRICS_PATH = "inference/tfbind8_mcmc_metrics_" + NAME_OF_RUN + ".npy"
 
-# Load data
+# Setup
 device = help.set_device()
 X_train = torch.load(X_TRAIN_PATH)
+
+# Load data
 random_samples = torch.load(RANDOM_SAMPLES_PATH)
 random_rewards = torch.load(RANDOM_REWARDS_PATH)
 gflow_samples = torch.load(GFLOW_SAMPLES_PATH)
 gflow_rewards = torch.load(GFLOW_REWARDS_PATH)
-# mcmc_samples = torch.load(MCMC_SAMPLES_PATH)
-# mcmc_rewards = torch.load(MCMC_REWARDS_PATH)
+mcmc_samples = torch.load(MCMC_SAMPLES_PATH)
+mcmc_rewards = torch.load(MCMC_REWARDS_PATH)
 
 # Get top 20% of random samples, then evaluate
-mask = torch.argsort(random_rewards, dim=0, descending=True)
-random_samples_sorted = random_samples[mask].squeeze()
-random_rewards_sorted = random_rewards[mask].squeeze()
-random_samples_top20 = random_samples_sorted[:int(len(random_samples_sorted) * 0.2)]
-random_rewards_top20 = random_rewards_sorted[:int(len(random_rewards_sorted) * 0.2)]
+# mask = torch.argsort(random_rewards, dim=0, descending=True)
+# random_samples_sorted = random_samples[mask].squeeze()
+# random_rewards_sorted = random_rewards[mask].squeeze()
+# random_samples_top20 = random_samples_sorted[:int(len(random_samples_sorted) * 0.2)]
+# random_rewards_top20 = random_rewards_sorted[:int(len(random_rewards_sorted) * 0.2)]
 
-random_perf, random_div, random_novel = evaluate_modelsampling(X_train,random_samples_top20,random_rewards_top20, print_stats = False)
-random_metrics = np.array([{"Performance": random_perf.detach().numpy().item(), "Diversity": random_div.detach().numpy().item(), "Novelty": random_novel.detach().numpy().item()}])
+# random_perf, random_div, random_novel = evaluate_modelsampling(X_train,random_samples_top20,random_rewards_top20, print_stats = False)
+# random_metrics = np.array([{"Performance": random_perf.detach().numpy().item(), "Diversity": random_div.detach().numpy().item(), "Novelty": random_novel.detach().numpy().item()}])
 
 # Get top 20% of MCMC samples, then evaluate
 # mask = torch.argsort(mcmc_rewards, dim=0, descending=True)
@@ -66,22 +69,17 @@ random_metrics = np.array([{"Performance": random_perf.detach().numpy().item(), 
 
 
 # Get top 20% of each gflow batch, then evaluate
-gflow_metrics = []
-for i in range(len(gflow_samples)):
-    mask = torch.argsort(gflow_rewards[i], dim=0, descending=True)
-    gflow_samples_sorted = gflow_samples[i][mask].squeeze()
-    gflow_rewards_sorted = gflow_rewards[i][mask].squeeze()
-    gflow_samples_top20 = gflow_samples_sorted[:int(len(gflow_samples_sorted) * 0.2)]
-    gflow_rewards_top20 = gflow_rewards_sorted[:int(len(gflow_rewards_sorted) * 0.2)]
-    gflow_perf, gflow_div, gflow_novel = evaluate_modelsampling(X_train,gflow_samples_top20,gflow_rewards_top20, print_stats = False)
-    gflow_metrics.append({"Performance": gflow_perf.detach().numpy().item(), "Diversity": gflow_div.detach().numpy().item(), "Novelty": gflow_novel.detach().numpy().item()})
-gflow_metrics = np.array(gflow_metrics)
-
+random_metrics = evaluate_batches(X_sampled = random_samples, y_sampled = random_rewards, X_train = X_train, print_stats=False)
+print("Random metrics completed")
+mcmc_metrics = evaluate_batches(X_sampled = mcmc_samples, y_sampled = mcmc_rewards, X_train = X_train, print_stats=False)
+print("MCMC metrics completed")
+gflow_metrics = evaluate_batches(X_sampled = gflow_samples, y_sampled = gflow_rewards, X_train = X_train, print_stats=False)
+print("GFlow metrics completed")
 
 
 # Save metrics
 np.save(RANDOM_METRICS_PATH, random_metrics, allow_pickle=True)
 np.save(GFLOW_METRICS_PATH, gflow_metrics, allow_pickle=True)
-#np.save(MCMC_METRICS_PATH, mcmc_metrics, allow_pickle=True)
+np.save(MCMC_METRICS_PATH, mcmc_metrics, allow_pickle=True)
 
 print("Evaluation complete. Saved metrics.")
