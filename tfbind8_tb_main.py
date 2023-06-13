a8dcc24a9d9a4065abd86dd9cea740b39f66cce3
jesperdlau <jesperdlau@gmail.com>
2023-06-13 09:42:07 +0200

    Removed unused imports

--- tfbind8_tb_main.py
+++ tfbind8_tb_main.py
@@ -3,16 +3,10 @@ import torch
 import numpy as np
 
 # Import scripts
-from MCMC_sampler import MCMCSequenceSampler
-from models.random_sampler import SequenceSampler
 from models.train_tb import train_tb
 from models.tfbind8_model_tb import GFlowNet
 from reward_functions.tf_bind_reward_1hot import TFBindReward1HOT
 from reward_functions import torch_helperfunctions as help
-from utilities.loss_plot import loss_plot
-from evaluation.evaluation import evaluate_modelsampling
-from utilities.transformer import Transformer
-from MCMC_light_sampler import MCMCLightSequenceSampler
 
 
 # Hyperparameters
@@ -20,7 +14,7 @@ NAME_OF_RUN = "100tb_test"
 
 HIDDEN_SIZE = 2048
 LEARNING_RATE = 10**-5
-NUM_EPISODES = 100
+NUM_EPISODES = 100 # Should be 5000 for tfbind8
 UPDATE_FREQ = 32
 DELTA = 0.001
 BETA = 3
