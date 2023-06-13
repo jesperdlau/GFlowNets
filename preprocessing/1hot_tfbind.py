efea75fac5f1588bc903b896da78e45396a98948
jesperdlau <jesperdlau@gmail.com>
2023-06-13 17:27:29 +0200

    Included test

--- preprocessing/1hot_tfbind.py
+++ preprocessing/1hot_tfbind.py
@@ -23,16 +23,20 @@ X = torch.tensor(X, dtype=torch.int64)
 y = torch.tensor(y, dtype=torch.float32).reshape(-1, 1)
 X_train = torch.tensor(X_train, dtype=torch.int64)
 y_train = torch.tensor(y_train, dtype=torch.float32).reshape(-1, 1)
+X_test = torch.tensor(X_test, dtype=torch.int64)
+y_test = torch.tensor(y_test, dtype=torch.float32).reshape(-1, 1)
 X_valid = torch.tensor(X_valid, dtype=torch.int64)
 y_valid = torch.tensor(y_valid, dtype=torch.float32).reshape(-1, 1)
 
 # 1hot encoding
 X_train = torch.nn.functional.one_hot(X_train,4).flatten(start_dim=1)
+X_test = torch.nn.functional.one_hot(X_test,4).flatten(start_dim=1)
 X_valid  = torch.nn.functional.one_hot(X_valid,4).flatten(start_dim=1)
 X       = torch.nn.functional.one_hot(X,4).flatten(start_dim=1)
 
 # correct types
 X_train = X_train.type("torch.FloatTensor")
+X_test  = X_test.type("torch.FloatTensor")
 X_valid  = X_valid.type("torch.FloatTensor")
 X       = X.type("torch.FloatTensor")
 
@@ -48,7 +52,10 @@ torch.save(X,"data/tfbind8/tfbind8_X.pt")
 torch.save(y,"data/tfbind8/tfbind8_y.pt")
 torch.save(X_train,"data/tfbind8/tfbind8_X_train.pt")
 torch.save(y_train,"data/tfbind8/tfbind8_y_train.pt")
+torch.save(X_test,"data/tfbind8/tfbind8_X_test.pt")
+torch.save(y_test,"data/tfbind8/tfbind8_y_test.pt")
 torch.save(X_valid,"data/tfbind8/tfbind8_X_valid.pt")
 torch.save(y_valid,"data/tfbind8/tfbind8_y_valid.pt")
+
 
 print("1-hot preprocessing complete")
