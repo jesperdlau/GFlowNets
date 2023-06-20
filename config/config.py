0de11a2f5f7768ea2182c0417ce45ea5869f6789
jesperdlau <jesperdlau@gmail.com>
2023-06-20 16:36:37 +0200

    changed values

--- config/config.py
+++ config/config.py
@@ -5,6 +5,6 @@ PWD = "./" # Path to GFlowNet in home directory
 PWD_WORK = "./" # Path to scratch directory
 # PWD = "/zhome/2e/b/169155/GFlowNet/" # Path to GFlowNet in home directory
 # PWD_WORK = "/work3/s214633/GFlowNet/" # Path to GFlowNet in scratch directory
-NAME_OF_RUN = "5k"
-NAME_OF_REWARD = "mlp"
+NAME_OF_RUN = "test"
+NAME_OF_REWARD = "test"
 
